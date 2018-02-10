18d2ddffd5e10f579f5d9e020e1f0e0de40146c2
Richard Rosales <theenumber3@gmail.com>
2018-02-10 03:33:16 -0800

    Add about and clients section

--- src/components/pages/Index/components/Onboarding.jsx
+++ src/components/pages/Index/components/Onboarding.jsx
@@ -5,6 +5,7 @@ import './Onboarding.css';
 import money from '../../../../../static/onboarding/moneyMarket.png';
 import music from '../../../../../static/onboarding/musicTranscript.png';
 import file from '../../../../../static/onboarding/sendFile.png';
+import map from '../../../../../static/onboarding/map.svg';
 
 // const propTypes = {
 //   logo: PropTypes.string.isRequired
@@ -38,6 +39,90 @@ class Onboarding extends Component {
         </section>
         <section className="video-section">
           <div className="video-row" />
+        </section>
+        <section className="other-section">
+          <div className="other-row">
+            <figure>
+              <img src="" alt="" />
+            </figure>
+            <ul>
+              <li>
+                <img src="" alt="" />
+                <h4>Publishing Administration</h4>
+                <p>
+                  Clear Digital handles worldwide registration and royalty collection from music publishers, performance
+                  royalty organizations, and digital retailers.
+                </p>
+              </li>
+              <li>
+                <img src="" alt="" />
+                <h4>Label Services</h4>
+                <p>
+                  ClearDigital can help record labels or independent artist move in the right direction. Our label
+                  services can help you review agreements, register for royalty collection (SoundExchange), manage
+                  artist and meet your production deadlines.
+                </p>
+              </li>
+              <li>
+                <img src="" alt="" />
+                <h4>Royalty Accounting</h4>
+                <p>
+                  Clear Digital offers a white label solution for any company that is required to process music
+                  royalties and participation statements.
+                </p>
+              </li>
+              <li>
+                <img src="" alt="" />
+                <h4>Localized Marketing</h4>
+                <p>
+                  By leveraging our team of experts and strategic partnerships, Clear Digital can help build your
+                  presence as a brand or artist.
+                </p>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className="about-section">
+          <div className="about-row">
+            <div>
+              <h3>
+                A new type of music company.<br /> Built by a global team.
+              </h3>
+              <p>
+                Clear Digital Music Group strives to provide clear and transparent music solutions. Whether you’re an
+                artist, composer, publisher or music label, Clear Digital Music Group has the expertise, tools and
+                relationships to get your music heard and ensure that you’re maximizing your time and revenue.
+              </p>
+            </div>
+            <figure>
+              <img src={map} alt="World Map" />
+              <span>Los Angeles, CA</span>
+            </figure>
+          </div>
+        </section>
+        <section className="clients-section">
+          <div className="section-title">
+            <h3>Our Clients.</h3>
+            <p>How to get your music distributed with ClearDigital.</p>
+          </div>
+          <div className="clients-row">
+            <div className="clients">
+              <img src="" alt="" />
+              <h6>Virlan Garcia</h6>
+            </div>
+            <div className="clients">
+              <img src="" alt="" />
+              <h6>ShowBusiness</h6>
+            </div>
+            <div className="clients">
+              <img src="" alt="" />
+              <h6>Jovanny Cadena</h6>
+            </div>
+            <div className="clients">
+              <img src="" alt="" />
+              <h6>Abe Records</h6>
+            </div>
+          </div>
         </section>
       </div>
     );
